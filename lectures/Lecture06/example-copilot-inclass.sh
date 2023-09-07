f4c3c01b2b2d7592f8d9fbd1dfd8d329cdd87497
Aaron Striegel <striegel@nd.edu>
2023-09-06 20:36:36 -0400

    Example from in-class on Monday

--- lectures/Lecture06/example-copilot-inclass.sh
+++ lectures/Lecture06/example-copilot-inclass.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 
+# Count the lines of code in a given file from the argument
+# and print the result to the terminal
+
+# Check if the user has provided a file name
+if [ $# -eq 0 ]; then
+    echo "Please provide a file name"
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f $1 ]; then
+    echo "File $1 does not exist"
+    exit 1
+fi
+
+# Count the lines of code in the file
+lines=$(wc -l < $1)
+
+# Print the result
+echo "File $1 has $lines lines of code"
+```
