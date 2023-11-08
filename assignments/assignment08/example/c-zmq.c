0df86b19b05be53ce7caf6919701c6643099a4f9
Aaron Striegel <striegel@nd.edu>
2023-11-08 11:03:31 -0500

    Updated code

--- assignments/assignment08/example/c-zmq.c
+++ assignments/assignment08/example/c-zmq.c
@@ -8,18 +8,36 @@
 
 int main (void)
 {
+    int rc;             /* Save the return code */
+
     printf ("Connecting to go irish server...\n");
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:ZMQ_SERVER_PORT");
+
+    char pszRequest[25];
+    sprintf(pszRequest, "tcp://localhost:%d", ZMQ_SERVER_PORT);
+
+    rc = zmq_connect (requester, pszRequest);
+
+    if(rc == 0)
+    {
+        /* If this printf is printed, the network is working */
+        printf("  Successfully connected\n");
+    }
+    else 
+    {
+        printf("  Network connection failed\n");
+        exit(-1);
+    }
 
     int request_nbr;
+
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
         char buffer [10];
         printf ("Sending go %d...\n", request_nbr);
-        zmq_send (requester, "go", 2, 0);
+        zmq_send (requester, "go", 3, 0);
         zmq_recv (requester, buffer, 10, 0);
-        printf ("Received irish %d\n", request_nbr);
+        printf ("Received %s (Message %d)\n", buffer, request_nbr);
     }
     zmq_close (requester);
     zmq_ctx_destroy (context);
